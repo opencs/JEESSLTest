74841d33a49d7e23bb9fa0ac19d04bd5562c9c71
Fabio Jun Takada Chino <fchino@opencs.com.br>
2019-06-25 20:45:38 -0300

    Initial version on GitHub.

---
+++ README.md
@@ -0,0 +1,57 @@
+# OpenCS's JEESSLTest
+
+## Description
+
+**OpenCS's JEESSLTest** is a small **Java EE 5+** web application designed to be
+used to test HTTP and HTTPS connections from withing a **Java EE Application Server**.
+
+It is specially useful to test connections in environments under heavy firewall
+protection and/or the use of intranet certificate chains.
+
+## How to build it
+
+The web application can be built by using **Apache Maven**, a compatible JDK and
+a Internet connection to get compile time dependencies (Java EE 5 API).
+
+Just run **mvn install** on the source directory and wait for it to end. The
+**war** package will be found inside the directory **target**.
+
+## How to use it
+
+Just deploy it on the target application server and access the published URL with
+any web browser.
+
+To test the connection, just put the target URL in the URL text box and press
+**Connect**. The result of the connection will be display right bellow.
+
+![screenshot](pics/screenshot.png)
+
+A short demonstration video can be found at https://youtu.be/TSvAAN8KKik
+
+## Compatibility
+
+This application was designed to be used with any **Java EE 5+** application server
+running on Java 1.5 or later. 
+
+This is the compatibility matrix with tested application servers:
+
+ Application Server | JDK | Result | Reported by
+ ------------------ | --- | ------ | -----------
+ Apache TomEE 7.1 | OpenJDK 8u212-b03 | OK | OpenCS
+ IBM WAS 6.1 | Built-in J9 | ? | OpenCS
+ IBM WAS 8.5.5.x | Built-in J9 | ? | OpenCS
+ IBM WAS 9.0.x | Built-in J9 | ? | OpenCS
+ RedHat JBoss 5.1 | Sun Microsystems JDK 1.5.0_22 | OK | OpenCS
+
+## Licensing
+
+This software is released under Apache License Version 2.0.
+
+## Contributions
+
+We are open to contributions from anyone. You can propose enhancements, report bugs and/or
+contribute with code using GitHub. Just keep in mind that the main objective of this
+project is to remain as simple and as compatible as possible.
+
+If you want to report compatiblity with other application servers, feel free to
+contact us. All contributions will be properly credited.
---
+++ src/main/java/br/com/opencs/ssltest/servlet/SSLTestServlet.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2019 Open Communications Security
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package br.com.opencs.ssltest.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.cert.Certificate;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SSLTestServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		this.doPost(req, resp);
+	}
+	
+	private void testURL(String urlString, PrintWriter out) throws IOException {
+		try {
+			out.printf("Creating URL instance for '%1$s'...", urlString);
+			URL url = new URL(urlString);
+			out.println("Done");
+			
+			out.printf("Opening the connection...");
+			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+			out.println("OK");
+			try {
+				out.printf("Get the return code...");
+				int returnCode = conn.getResponseCode();
+				out.println("OK");
+				out.printf("The return code is %1$d\n", returnCode);
+				
+				if (conn instanceof HttpsURLConnection) {
+					out.printf("\n\nHTTPS connection:\n");
+					HttpsURLConnection ssl = (HttpsURLConnection)conn;
+					out.printf("Cipher: %1$s\n", ssl.getCipherSuite());
+	
+					Certificate [] certs = ssl.getServerCertificates();
+					for (int i = 0; i < certs.length; i++) {
+						out.println("======================================================================");
+						out.println(certs[i].toString());
+						out.println("======================================================================");
+					}
+				}
+			} finally {
+				out.printf("Disconnecting...");
+				conn.disconnect();
+				out.println("OK");
+			}
+		} catch (Exception e) {
+			out.println("\n\n#####\nError\n#####\n");
+			e.printStackTrace(out);
+		}		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		resp.setContentType("text/plain");
+		
+		String url = req.getParameter("url"); 
+		if (url != null) {
+			testURL(url, resp.getWriter());
+		} else {
+			resp.getWriter().print("Missing URL!");
+		}
+		resp.getWriter().close();		
+	}
+
+}
